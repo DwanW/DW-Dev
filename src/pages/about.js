e25e121f6ab34ac3b629c1dff401da3592744c77
DwanW <kunvang@yahoo.com>
2020-11-19 00:00:14 -0700

    finishing the UI/UX part of contact and about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,53 @@
 import React from "react"
-import { Link } from "gatsby"
+import styled from 'styled-components'
+import { useStaticQuery, graphql } from 'gatsby'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Image from '../components/image'
 
-const AboutPage = ({location}) => (
+const AboutTitle = styled.div`
+font-weight: 600;
+font-size: 24px;
+line-height: 28px;
+text-align: center;
+margin-top: 50px;
+`
+
+const AboutDescription = styled.div`
+width: 400px;
+margin: 40px auto;
+`
+
+const AboutPage = ({location}) => {
+  const data = useStaticQuery(graphql`
+  query {
+    allAboutJson {
+      nodes {
+        title
+        description
+        image
+      }
+    }
+  }
+  `)
+  return(
   <Layout location={location}>
     <SEO title="About" />
-    <h1>About Page</h1>
-    <Link to="/">Go back to the cover</Link>
+    <AboutTitle>In Short</AboutTitle>
+    <AboutDescription>we are group of passionate developers who love technology and modern innovation. Our mission is to bring the most value in web development to people who needs it.</AboutDescription>
+    {
+      data.allAboutJson.nodes.map((node, idx) => {
+        return(
+          <div key={idx}>
+            <Image filename={node.image} wrapperStyle={{width: 400, margin: "auto"}}/>
+            <AboutTitle>{node.title}</AboutTitle>
+            <AboutDescription>{node.description}</AboutDescription>
+          </div>
+        )
+      })
+    }
   </Layout>
-)
+)}
 
 export default AboutPage
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,15 +1,147 @@
 import React from "react"
-import { Link } from "gatsby"
+import styled from 'styled-components'
+import { useStaticQuery, graphql } from 'gatsby'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import CustomInput from "../components/CustomInput"
+import CustomButton from "../components/CustomButton"
 
-const ContactPage = ({location}) => (
+const FAQSection = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+`
+
+const ContactSectionTitle = styled.div`
+color: #3182ce;
+font-size: 24px;
+font-weight: 600;
+margin: 20px auto;
+text-align: center;
+`
+
+const FAQItem = styled.div`
+margin: 20px 0;
+`
+
+const QuestionContainer = styled.div`
+color: #343434;
+font-size: 22px;
+font-weight: 600;
+`
+
+const AnswerContainer = styled.div`
+margin: 5px 0;
+`
+
+const ContactSection = styled.div`
+display: flex;
+flex-direction: column;
+align-items:center;
+`
+
+const ContactCard = styled.div`
+width: 300px;
+`
+
+const ContactTarget = styled.div`
+text-align: center;
+`
+
+const ContactAddress = styled.div`
+text-align: center;
+`
+
+const FormSection = styled.div`
+margin-top: 40px;
+`
+
+const FormContainer = styled.div`
+`
+
+const RowContainer = styled.div`
+display: flex;
+width: 100%;
+`
+
+const CustomTextArea = styled.textarea`
+background: #FFFFFF;
+border-radius: 6px;
+padding: 5px 10px;
+border: none;
+transition: all 0.3s;
+font-weight: 600;
+font-size: 20px;
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+color: #343434;
+margin-top: 15px;
+resize: none;
+
+&::placeholder {
+font-weight: 600;
+font-size: 20px; 
+color: #343434;
+opacity: 0.4;
+}
+&:active {
+  outline: none;
+}
+&:focus {
+  outline: none;
+}
+`
+
+const ContactPage = ({location}) => {
+  const data = useStaticQuery(graphql`
+  query {
+    allFaqJson {
+      nodes {
+        question
+        answer
+      }
+    }
+  }
+  `)
+
+  return(
   <Layout location={location}>
     <SEO title="Contact" />
-    <h1>Contact Page</h1>
-    <Link to="/">Go back to the cover</Link>
+    <FAQSection>
+      <ContactSectionTitle>Have any questions?</ContactSectionTitle>
+      {
+        data.allFaqJson.nodes.map((node, idx) => {
+          return (
+            <FAQItem key={idx}>
+              <QuestionContainer>{node.question}</QuestionContainer>
+              <AnswerContainer>{node.answer}</AnswerContainer>
+            </FAQItem>
+          )
+        })
+      }
+    </FAQSection>
+
+    <ContactSection>
+      <ContactSectionTitle>Contact Us</ContactSectionTitle>
+      <ContactCard>
+        <ContactTarget>General Contact</ContactTarget>
+        <ContactAddress>info@DWdev.com</ContactAddress>
+      </ContactCard>
+    </ContactSection>
+
+    <FormSection>
+      <ContactSectionTitle>Send us a Message</ContactSectionTitle>
+      <FormContainer>
+        <RowContainer>       
+          <CustomInput placeholder="Email" type="email" style={{ flexGrow: 1, marginRight: 20}}/>
+          <CustomInput placeholder="Name" type="text" style={{ flexGrow: 1}}/>
+        </RowContainer>
+        <CustomTextArea placeholder="Message" type="text" style={{ flexGrow: 1, width: "100%", height: "150px"}}/>
+        <CustomButton inverted style={{padding: "5px 40px", fontSize: 20}}>Submit</CustomButton>
+      </FormContainer> 
+    </FormSection>
   </Layout>
-)
+)}
 
 export default ContactPage
