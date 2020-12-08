fc5a7a23240fcda3173142bf7688205b35b01ee4
DwanW <kunvang@yahoo.com>
2020-12-07 18:15:47 -0700

    adding netlify cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-transition-link`,
     {
