ce8c8be4e40e2339e8054183ce1810751e8d92c8
DwanW <kunvang@yahoo.com>
2021-01-14 23:53:05 -0700

    adding palette

---
+++ src/assets/theme.js
@@ -0,0 +1,12 @@
+const theme =  {
+    primaryBlueDark: "#215688",
+    primaryBlueReg: "#3182CE",
+    primaryBlueLight: "#81AED8",
+    uiBlack: "#273C50",
+    uiGrayDark: "#A2B4C5",
+    uiGrayLight: "#DFEDFA",
+    accentOrange: "#ED892D",
+    accentViolet: "#EE418A"
+}
+
+export default theme
